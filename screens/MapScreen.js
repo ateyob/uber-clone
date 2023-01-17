f79c713ef98b2697bd2cfd0d924b04a387862ad5
Eyob Kefyalew <ateyob@gmail.com>
2023-01-17 16:47:25 +0300

    homepage started

---
+++ screens/MapScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const MapScreen = () => {
+  return (
+    <View>
+      <SafeAreaView>
+        <Text>Here is the map stuff......</Text>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
